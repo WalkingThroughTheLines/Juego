cca5de06e928b347aa3e439f7df62b88c79abd59
FrJesusOrtegaLucena <franjesusorlu@gmail.com>
2021-02-24 18:44:18 +0100

    añadiendo las pruebas ya hechas

--- src/juegoPruebas/Ventana.java
+++ src/juegoPruebas/Ventana.java
@@ -1,5 +1,34 @@
 package juegoPruebas;
 
-public class Ventana {
+import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Ventana extends JFrame {
+
+
+	public Ventana(){
+		setSize(500,500);
+		setTitle("Walking Through The Lines");
+		setLocationRelativeTo(null);
+		setMinimumSize(new Dimension(200,200));
+		iniciarPaneles();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	private void iniciarPaneles() {
+		JPanel panel1= new JPanel();
+		this.getContentPane().add(panel1);
+		JLabel etiqueta1 = new JLabel("Bienvenidos al juegardo ");
+		JLabel etiqueta2 = new JLabel("Walking Through The Lines");
+		etiqueta2.setForeground(Color.darkGray);
+		
+		panel1.add(etiqueta1);
+		panel1.add(etiqueta2);
+	}
+	
+	
 }
